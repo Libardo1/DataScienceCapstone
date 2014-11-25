a28457f7c228057b47a4b5c2479839ea5b3e4a8a
datasciencespm <mspcvsp@gmail.com>
2014-11-25 16:45:17 -0500

    Updated shiny application implementation

--- shinyApplication/server.R
+++ shinyApplication/server.R
@@ -1,4 +1,63 @@
+library(rJava)
+library(RWeka)
+library(R.utils)
+library(stringi)
+library(stringr)
 library(shiny)
+library(textcat)
+library(tm)
+library(markovchain)
 
-shinyServer(function(input, output) { 
+# http://stackoverflow.com/questions/9934856/removing-non-ascii-characters-from-data-files
+# http://stackoverflow.com/questions/18153504/removing-non-english-text-from-corpus-in-r-using-tm
+removeNonASCII <-
+    content_transformer(function(x) iconv(x, "latin1", "ASCII", sub=""))
+
+# http://stackoverflow.com/questions/14281282/
+# how-to-write-custom-removepunctuation-function-to-better-deal-with-unicode-cha
+#
+# http://stackoverflow.com/questions/8697079/remove-all-punctuation-except-apostrophes-in-r
+customRemovePunctuation <- content_transformer(function(x) {
+    x <- gsub("[[:punct:]]"," ",tolower(x))
+    return(x)
+})
+
+readBlackList <- function(blackListFile) {
+    blackList <- read.csv(blackListFile,header=FALSE,skip=4)
+    blackList <- blackList[,2]
+    blackList <- gsub(",","",blackList)
+    blackList <- gsub(" ","",blackList)
+    blackList <- gsub("[0-9]+","",blackList)
+    blackList <- gsub("[\\.-]","",blackList)
+    blackList <- blackList[!grepl("^a$",blackList)]
+    blackList <- unique(blackList[blackList != ""])
+    return(blackList)
+}
+
+preprocessTextInput <- function(textInput) {
+    textInputCorpus <- Corpus(VectorSource(textInput))
+    
+    textInputCorpus <- tm_map(textInputCorpus, removeNonASCII)
+    
+    textInputCorpus <- tm_map(textInputCorpus, customRemovePunctuation)
+    
+    textInputCorpus <- tm_map(textInputCorpus, removeNumbers)
+    
+    textInputCorpus <- tm_map(textInputCorpus, stripWhitespace)
+    
+    textInputCorpus <- tm_map(textInputCorpus, removeWords, blackList)
+    
+    predictorInput <- as.character(textInputCorpus[[1]])
+}
+
+blackList <- readBlackList("./Terms-to-Block.csv")
+
+load(file="./transitionMatrix.RData")
+
+shinyServer(function(input, output) {
+    output$text1 <- renderText({ 
+        predictorInput <- preprocessTextInput(input$currentPhrase)
+        
+        paste("Predictor input", predictorInput)
+    })
 })
--- shinyApplication/ui.R
+++ shinyApplication/ui.R
@@ -5,10 +5,17 @@ shinyUI(pageWithSidebar(
     headerPanel("English Language Text Predictor"),
     
     sidebarPanel(
+        fluidRow(sliderInput("numberOfTerms",
+                             label="Number of terms to suggest",
+                             min=1,
+                             max=5,
+                             value=3,
+                             step=1),
         submitButton(text = "Apply Changes", icon = NULL)
-    ),
+    )),
     
     mainPanel(
+        textInput("currentPhrase", "Enter Phrase", value = ""),
+        textOutput("text1")
     )
 ))
-    
